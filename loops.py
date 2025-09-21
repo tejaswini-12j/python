16bfd250bf4f3024d18e7f1a699fdcbc437cf087
tejaswini-12j <tejaswinijampana82@gmail.com>
2025-09-21 16:19:26 +0530

    python loops

---
+++ loops.py
@@ -0,0 +1,11 @@
+# list1=[1,2,3,4,5,6,7,8,9,10]
+# list2=[]
+# for  num in list1: 
+#     if num%2==0:
+#         list2+=[num]
+# print(list2)
+
+# print(list(range(1,101,2)))
+#generate values using range:
+for i in range(1,10):
+    print(i)
